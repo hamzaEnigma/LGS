65689d13fb5857618397cf3c7d29638a87150dfb
hamzaEnigma <hamzasomai94@gmail.com>
2024-04-15 21:08:57 +0200

    commit: add git ignore

--- LGS/obj/Debug/net7.0/LGS.MvcApplicationPartsAssemblyInfo.cs
+++ LGS/obj/Debug/net7.0/LGS.MvcApplicationPartsAssemblyInfo.cs
@@ -11,13 +11,8 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("LGS")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9992de556da7cddeaf5ba92017493c3584bb8481")]
-[assembly: System.Reflection.AssemblyProductAttribute("LGS")]
-[assembly: System.Reflection.AssemblyTitleAttribute("LGS")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Microsoft.AspNetCore.OpenApi")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Swashbuckle.AspNetCore.SwaggerGen")]
 
 // Généré par la classe MSBuild WriteCodeFragment.
 
